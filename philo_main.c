68087eea7613de407258b83234ab5320c9abc1d6
enijakow <enijakow@student.42heilbronn.de>
2022-02-09 13:02:29 +0100

    Added flank calculation

--- philo_main.c
+++ philo_main.c
@@ -6,7 +6,7 @@
 /*   By: enijakow <enijakow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/17 18:27:39 by enijakow          #+#    #+#             */
-/*   Updated: 2022/02/08 19:11:00 by enijakow         ###   ########.fr       */
+/*   Updated: 2022/02/08 19:42:10 by enijakow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,17 +52,24 @@ void	*philo_launch(void *void_philo)
 	unsigned long	time;
 	struct s_philo	*philo;
 	unsigned int	wait_mul;
+	unsigned int	flanks;
 
 	philo = void_philo;
 	while (!philo->philos->matrix_start)
 		usleep(50);
+	if (philo->philos->table_size % 2)
+		flanks = 3;
+	else
+		flanks = 2;
+	if (philo % 2 == 0)
+		wait_mul = 0;
+	else if (philo % 2 == 1)
+		wait_mul = philo->philos->stats.time_to_eat / 2;
 	wait_mul = philo->num % 2;
-	if (philo->num == 0 && philo->philos->table_size % 2)
-		wait_mul = 2;
 	time = philo->philos->start_time;
 	philo->last_eat = time;
 	philo->last_event = time;
-	philo->next_event = time + wait_mul * philo->philos->stats.time_to_eat / 2;
+	philo->next_event = time + wait_mul * (philo->philos->stats.time_to_eat / flanks);
 	philo_main(philo, time);
 	if (philo->has_forks)
 		philo_drop_forks(philo);
