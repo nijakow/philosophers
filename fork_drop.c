55349e6c53204849e739ad081b6432cd2ee9a4d7
enijakow <enijakow@student.42heilbronn.de>
2022-02-10 16:56:42 +0100

    The forks now set their values

--- fork_drop.c
+++ fork_drop.c
@@ -6,7 +6,7 @@
 /*   By: enijakow <enijakow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/17 15:20:50 by enijakow          #+#    #+#             */
-/*   Updated: 2022/02/08 16:37:32 by enijakow         ###   ########.fr       */
+/*   Updated: 2022/02/10 16:56:29 by enijakow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,6 @@
 
 void	fork_drop(struct s_fork *fork)
 {
+	fork->value = false;
 	pthread_mutex_unlock(&fork->mutex);
 }
--- fork_take.c
+++ fork_take.c
@@ -6,7 +6,7 @@
 /*   By: enijakow <enijakow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/17 15:17:26 by enijakow          #+#    #+#             */
-/*   Updated: 2022/02/08 16:37:45 by enijakow         ###   ########.fr       */
+/*   Updated: 2022/02/10 16:56:24 by enijakow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,5 @@
 void	fork_take(struct s_fork *fork)
 {
 	pthread_mutex_lock(&fork->mutex);
+	fork->value = true;
 }
